5b6a6ab57845ebacd39239bfbbcb13fc12dd45cb
Myself <me@myk20.local>
2019-06-14 12:07:42 +0530

    fixed var

--- sg/main.tf
+++ sg/main.tf
@@ -1,14 +1,14 @@
 # Use vpc_id as data source to get cidr block
 data "aws_vpc" "selected" {
-  id = var.vpc_id
-}
+   id = var.vpc_id
+ }
 
 
 resource "aws_security_group" "pgdbsg" {
   name        = "pgdbsg"
   description = "RDS postgres servers"
   vpc_id      = var.vpc_id
-
+ 
   ingress {
     from_port   = 5432
     to_port     = 5432
@@ -16,7 +16,7 @@ resource "aws_security_group" "pgdbsg" {
     cidr_blocks = [data.aws_vpc.selected.cidr_block]
   }
 
-  # Allow all outbound traffic.
+ # Allow all outbound traffic.
   egress {
     from_port   = 0
     to_port     = 0
