88ccec27605fe9ba6f864336f25010e41bf6ba58
Myself <me@myk20.local>
2019-06-16 09:53:05 +0530

    deleted unused subdir

--- modules/sshkeypair/main.tf
+++
@@ -1,9 +0,0 @@
-# Create keypair using $HOME/.ssh/id_rsa.pub
-resource "aws_key_pair" "sshkeypair" {
-  key_name   = "sshkey"
-  public_key = file(var.sshpubkey)
-}
-
-output "sshkeyname" {
-  value = aws_key_pair.sshkeypair.key_name
-}
